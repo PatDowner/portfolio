575dbf21e8fab5678bed9d05c5bf26b9b6aa2b3f
Pat Downer <patricia.downer@gmail.com>
2020-09-23 22:33:04 -0700

    Updates

---
+++ precache-manifest.7f9157d91d497987744b371fec5259cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcb7ecdfacb8adaa4da474ef49185ee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3860fd285a7cedecee9",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a8de0589b96deaa12fef",
+    "url": "/portfolio/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f3860fd285a7cedecee9",
+    "url": "/portfolio/static/js/2.1aeceb0f.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/portfolio/static/js/2.1aeceb0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8de0589b96deaa12fef",
+    "url": "/portfolio/static/js/main.119740b2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f0cc6eae513ea3b4507742acac7dc968",
+    "url": "/portfolio/static/media/Poke_Ball.f0cc6eae.png"
+  },
+  {
+    "revision": "c12307f353f4af5776c456a0b9c1f970",
+    "url": "/portfolio/static/media/burger.c12307f3.jpg"
+  },
+  {
+    "revision": "f52a4837c3fba237eebfcf85efae01f5",
+    "url": "/portfolio/static/media/employees.f52a4837.jpg"
+  },
+  {
+    "revision": "f25f7d0c8de1f6403a749f2c1c0c8e1f",
+    "url": "/portfolio/static/media/me.f25f7d0c.jpg"
+  },
+  {
+    "revision": "d3bb3ad71a3e9bfd49978ddd0e2f3cb5",
+    "url": "/portfolio/static/media/quiz.d3bb3ad7.jpg"
+  },
+  {
+    "revision": "477430b54e229038707b455cbf505aaf",
+    "url": "/portfolio/static/media/weatherApp.477430b5.png"
+  }
+]);
